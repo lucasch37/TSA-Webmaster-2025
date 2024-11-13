c2d3e1bb26360768d943054d1fee97fe6e962aa9
AliMacky <ali.macky@outlook.com>
2024-11-13 14:02:38 -0800

    added font

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 import ThemeToggle from "@/components/themeToggle";
+import localFont from 'next/font/local'
+
+const myFont = localFont({ src: './fonts/HomemadeApple-Regular.ttf' })
+
 
 export default function Home() {
     return (
-        <div>
-            <ThemeToggle />
+        <div className={`p-14`}>
+            <p className={`text-center ${myFont.className} text-6xl`}>Sprout & About</p>
         </div>
     );
 }
