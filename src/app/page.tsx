41b645e764b8bc585ce94321a0d6913b2f685322
Lucas Chen <130620394+lucasch37@users.noreply.github.com>
2024-12-14 17:42:19 -0800

    added hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,68 @@
-import ThemeToggle from "@/components/themeToggle";
-import localFont from 'next/font/local'
-
-const myFont = localFont({ src: './fonts/HomemadeApple-Regular.ttf' })
-
+import Navbar from "@/components/navbar";
+import { Marquee } from "@/components/ui/marquee";
+import { MenuSquare } from "lucide-react";
+import Image from "next/image";
 
 export default function Home() {
     return (
-        <div className={`p-14`}>
-            <p className={`text-center ${myFont.className} text-6xl`}>Sprout & About</p>
+        <div className="flex flex-col">
+            <Navbar />
+            <div>
+                <div className="container grid grid-cols-5 mt-2">
+                    <div className="flex flex-col mt-32 col-span-3">
+                        <div className="text-8xl 2xl:text-[5.5rem] lg:text-[4.5rem] font-bold leading-tight text-primary tracking-tighter">
+                            <div className="flex gap-8 items-center">
+                                NATURE'S{" "}
+                                <img
+                                    src="/small-dot.svg"
+                                    alt=""
+                                    className="rotate-45 w-[5rem] h-[5rem]"
+                                />
+                            </div>{" "}
+                            FINEST FLAVORS
+                        </div>
+                        <div className="text-primary text-lg font-medium max-w-lg mt-4">
+                            Experience Asian cuisine with our beautifully
+                            crafted restaurant website. From mouth-watering
+                            menus to photo galleries, we show the essence of
+                            Asian flavors.
+                        </div>
+                        <div className="mt-8">
+                            <button className="bg-primary text-white py-4 px-8 rounded-full flex gap-3 items-center">
+                                <MenuSquare />
+                                View Our Menu
+                            </button>
+                        </div>
+                    </div>
+                    <div className="relative h-fit -ml-32 col-span-2">
+                        <img src="/hero-img.png" alt="" className="w-full" />
+                        <img
+                            src="/leaf.png"
+                            alt=""
+                            className="absolute top-4 right-12 h-20 w-20 -rotate-90"
+                        />
+                        <img
+                            src="/leaf2.png"
+                            alt=""
+                            className="absolute bottom-4 right-12 h-24 rotate-[195deg]"
+                        />
+                        <img
+                            src="/leaf.png"
+                            alt=""
+                            className="absolute top-4 left-12 h-20 w-20 rotate-[190deg]"
+                        />
+                    </div>
+                </div>
+                <div className="w-full mt-8">
+                    <div className="border-y border-primary text-primary text-lg py-2">
+                        <Marquee className="[--duration:5s]">
+                            <div>100% PLANT-BASED</div>
+                            <div>🌿</div>
+                        </Marquee>
+                    </div>
+                </div>
+                <div className="h-48"></div>
+            </div>
         </div>
     );
 }
---
+++ src/components/navbar.tsx
@@ -0,0 +1,50 @@
+import {
+    MessageCircleQuestion,
+    ShoppingBag,
+    SquareMenu,
+    Truck,
+    User,
+} from "lucide-react";
+import Link from "next/link";
+import React from "react";
+
+type Props = {};
+
+const Navbar = async (props: Props) => {
+    return (
+        <div className="inset-x-0 top-0 z-0 h-fit py-6">
+            <div className="flex items-center justify-between h-full gap-2 container">
+                <Link href={"/"} className="flex items-center gap-2 w-32">
+                    <div className="font-bold text-2xl text-center text-primary font-homemade-apple">
+                        Sprout &<br />
+                        About
+                    </div>
+                </Link>
+                <div className="border border-primary rounded-full py-3 px-6 flex gap-8 text-primary">
+                    <div className="flex gap-2 items-center">
+                        <SquareMenu size={20} /> MENU
+                    </div>
+                    <div className="flex gap-2 items-center">
+                        <MessageCircleQuestion size={20} /> ABOUT
+                    </div>
+                    <div className="flex gap-2 items-center">
+                        <Truck size={20} /> ORDER
+                    </div>
+                    <div className="flex gap-2 items-center">
+                        <SquareMenu size={20} /> MENU
+                    </div>
+                </div>
+                <div className="flex items-center gap-4 w-32">
+                    <div className="rounded-full border border-primary text-primary p-3">
+                        <User />
+                    </div>
+                    <div className="rounded-full border border-primary text-primary p-3">
+                        <ShoppingBag />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
