5fa3f48c80f265a3439c27b3e7edca037932311a
Lucas Chen <130620394+lucasch37@users.noreply.github.com>
2024-12-15 19:23:52 -0800

    css tweaks

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ import {motion} from "framer-motion";
 export default function Home(): JSX.Element {
     return (
         <div className="flex flex-col">
-            <Navbar/>
+            <Navbar />
             <div>
                 <div className="container grid grid-cols-5 mt-2">
                     <div className="flex flex-col justify-center col-span-3">
@@ -16,48 +16,54 @@ export default function Home(): JSX.Element {
                             initial={{opacity: 0, scale: 0.9}}
                             animate={{opacity: 1, scale: 1}}
                             transition={{type: "spring", stiffness: 80}}
-                            className="title pb-[10.5rem] text-8xl 2xl:text-[5.5rem] lg:text-[4.5rem] font-bold leading-tight text-green-600 tracking-tighter">
-                            {
-                                [...Array(2)].map((_, i) =>
-                                    <div className="title-content" key={i}>
-                                        <div className="flex gap-8 items-center">
-                                            NATURE'S{" "}
-                                            <img
-                                                src="/small-dot.svg"
-                                                alt=""
-                                                className="rotate-45 rounded-full overflow-hidden w-[5rem] h-[5rem] pointer-events-none"
-                                            />
-                                        </div>
-                                        {" "}
-                                        FINEST FLAVORS
-                                    </div>
-                                )
-                            }
+                            className={`title font-bold leading-tight text-green-600 tracking-tighter
+                                2xl:pb-[13rem] pb-[10.5rem] 
+                                text-8xl 2xl:text-[5.5rem] text-[4.5rem]
+                                `}
+                        >
+                            {[...Array(2)].map((_, i) => (
+                                <div className="title-content" key={i}>
+                                    <div className="flex gap-8 items-center">
+                                        NATURE'S{" "}
+                                        <img
+                                            src="/small-dot.svg"
+                                            alt=""
+                                            className="rotate-45 rounded-full overflow-hidden w-[5rem] h-[5rem] pointer-events-none"
+                                        />
+                                    </div>{" "}
+                                    FINEST FLAVORS
+                                </div>
+                            ))}
                         </motion.div>
-                        <motion.div initial={{opacity: 0, scale: 0.9}}
-                                    animate={{opacity: 1, scale: 1}}
-                                    transition={{type: "spring", stiffness: 80, delay: 0.15}}
-                                    className="text-primary text-lg font-medium max-w-lg mt-4">
+                        <motion.div
+                            initial={{opacity: 0, scale: 0.9}}
+                            animate={{opacity: 1, scale: 1}}
+                            transition={{type: "spring", stiffness: 80, delay: 0.15}}
+                            className="text-primary text-lg font-medium max-w-lg mt-4"
+                        >
                             Experience Asian cuisine with our beautifully crafted
                             restaurant website. From mouth-watering menus to photo
                             galleries, we show the essence of Asian flavors.
                         </motion.div>
-                        <motion.div initial={{opacity: 0, scale: 0.9}}
-                                    animate={{opacity: 1, scale: 1}}
-                                    transition={{type: "spring", stiffness: 80, delay: 0.25}}
-                                    className="mt-8">
-                            <button
-                                className="bg-primary hover:bg-green-800 transition ease-in-out duraiton-300 text-white py-4 px-8 rounded-full flex gap-3 items-center">
-                                <MenuSquare/>
+                        <motion.div
+                            initial={{opacity: 0, scale: 0.9}}
+                            animate={{opacity: 1, scale: 1}}
+                            transition={{type: "spring", stiffness: 80, delay: 0.25}}
+                            className="mt-8"
+                        >
+                            <button className="bg-primary hover:bg-green-800 transition ease-in-out duraiton-300 text-white py-4 px-8 rounded-full flex gap-3 items-center">
+                                <MenuSquare />
                                 Explore Our Menu
                             </button>
                         </motion.div>
                     </div>
-                    <motion.div initial={{opacity: 0, scale: 0.9}}
-                                animate={{opacity: 1, scale: 1}}
-                                transition={{type: "spring", stiffness: 80}}
-                                className="relative h-fit -ml-32 col-span-2">
-                        <img src="/hero-img.png" alt="" className="w-full"/>
+                    <motion.div
+                        initial={{opacity: 0, scale: 0.9}}
+                        animate={{opacity: 1, scale: 1}}
+                        transition={{type: "spring", stiffness: 80}}
+                        className="relative h-fit xl:-ml-32 -ml-12 col-span-2"
+                    >
+                        <img src="/hero-img.png" alt="" className="w-full" />
                         <img
                             src="/leaf.png"
                             alt=""
@@ -75,10 +81,12 @@ export default function Home(): JSX.Element {
                         />
                     </motion.div>
                 </div>
-                <motion.div initial={{opacity: 0, x: 40}}
-                            animate={{opacity: 1, x: 0}}
-                            transition={{ease: "linear", duration: 0.5, delay: 0.35}}
-                            className="w-full mt-8">
+                <motion.div
+                    initial={{opacity: 0, x: 40}}
+                    animate={{opacity: 1, x: 0}}
+                    transition={{ease: "linear", duration: 0.5, delay: 0.35}}
+                    className="w-full mt-8"
+                >
                     <div className="border-y border-primary text-primary text-lg py-2">
                         <Marquee className="[--duration:5s]">
                             <div>100% PLANT-BASED</div>
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -11,40 +11,46 @@ const Navbar = (): JSX.Element => {
             <div className="flex items-center justify-between h-full gap-2 container">
                 <Link href={"/"} className="flex items-center gap-2 w-32">
                     <div className="font-bold text-2xl text-center text-primary font-homemade-apple">
-                        Sprout &<br/>
+                        Sprout &<br />
                         About
                     </div>
                 </Link>
-                <motion.div initial={{scale: 0.9}}
-                            animate={{scale: 1}}
-                            whileHover={{scale: 1.05}}
-                            transition={{type: "spring", stiffness: 80}}
-                            className="border border-primary rounded-full py-2 px-6 flex gap-8 text-primary">
+                <motion.div
+                    initial={{scale: 0.9}}
+                    animate={{scale: 1}}
+                    whileHover={{scale: 1.05}}
+                    transition={{type: "spring", stiffness: 80}}
+                    className="border border-primary rounded-full py-2 px-6 flex gap-8 text-primary"
+                >
                     <button className="flex gap-2 items-center nav-link">
-                        <SquareMenu size={20}/> MENU
+                        <SquareMenu size={20} /> MENU
                     </button>
                     <button className="flex gap-2 items-center nav-link">
-                        <MessageCircleQuestion size={20}/> ABOUT
+                        <MessageCircleQuestion size={20} /> ABOUT
                     </button>
                     <button className="flex gap-2 items-center nav-link">
-                        <Truck size={20}/> ORDER
+                        <Truck size={20} /> ORDER
                     </button>
                     <button className="flex gap-2 items-center nav-link">
-                        <SquareMenu size={20}/> MENU
+                        <SquareMenu size={20} /> MENU
                     </button>
                 </motion.div>
                 <div className="flex items-center gap-4 w-32">
-                    <motion.button initial={{scale: 0.9}}
-                                   animate={{scale: 1}}
-                                   transition={{type: "spring", stiffness: 80}}
-                                   className="rounded-full border border-primary hover:bg-primary hover:text-white transition ease-in-out duration-150 text-primary p-3">
-                        <User/>
+                    <motion.button
+                        initial={{scale: 0.9}}
+                        animate={{scale: 1}}
+                        transition={{type: "spring", stiffness: 80}}
+                        className="rounded-full border border-primary hover:bg-primary hover:text-white transition ease-in-out duration-150 text-primary p-3"
+                    >
+                        <User />
                     </motion.button>
-                    <motion.button initial={{scale: 0.9}}
-                                   animate={{scale: 1}}
-                                   transition={{type: "spring", stiffness: 80}}
-                                   className="rounded-full border border-primary hover:bg-primary hover:text-white transition ease-in-out duration-150 text-primary p-3">
-                        <ShoppingBag/>
+                    <motion.button
+                        initial={{scale: 0.9}}
+                        animate={{scale: 1}}
+                        transition={{type: "spring", stiffness: 80}}
+                        className="rounded-full border border-primary hover:bg-primary hover:text-white transition ease-in-out duration-150 text-primary p-3"
+                    >
+                        <ShoppingBag />
                     </motion.button>
                 </div>
             </div>
