c7ab02dde15abd1cfe2e36a23d4578076674c609
AliMacky <ali.macky@outlook.com>
2025-01-22 23:21:22 -0800

    references page

---
+++ src/app/references/page.tsx
@@ -0,0 +1,255 @@
+import React from "react";
+
+export default function ReferencesPage(): React.JSX.Element {
+    return (
+        <div className="container mx-auto px-4 py-8">
+            <h1 className="text-4xl font-bold mb-8">References</h1>
+
+            <section className="mb-12">
+                <h2 className="text-2xl font-semibold mb-4">Recipes</h2>
+                <ul className="list-disc pl-5 space-y-2">
+                    <li>
+                        <a
+                            href="https://www.inspiredtaste.net/48013/chimichurri-cauliflower-steaks/"
+                            className="text-blue-600 hover:underline"
+                        >
+                            Chimichurri Cauliflower Steaks - Inspired Taste
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            href="https://www.foodnetwork.com/recipes/food-network-kitchen/eggplant-parmesan-recipe-2008982"
+                            className="text-blue-600 hover:underline"
+                        >
+                            Eggplant Parmesan - Food Network Kitchen
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            href="https://www.foodnetwork.com/recipes/food-network-kitchen/the-best-caesar-salad-8037173"
+                            className="text-blue-600 hover:underline"
+                        >
+                            The Best Caesar Salad - Food Network Kitchen
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            href="https://www.thepioneerwoman.com/food-cooking/recipes/a11831/black-bean-burger/"
+                            className="text-blue-600 hover:underline"
+                        >
+                            Black Bean Burger - The Pioneer Woman
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            href="https://cookieandkate.com/best-vegetable-lasagna-recipe/"
+                            className="text-blue-600 hover:underline"
+                        >
+                            Best Vegetable Lasagna - Cookie and Kate
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            href="https://www.tasteofhome.com/recipes/vegetarian-stuffed-peppers/"
+                            className="text-blue-600 hover:underline"
+                        >
+                            Vegetarian Stuffed Peppers - Taste of Home
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            href="https://veganheaven.org/recipe/spaghetti-with-bean-balls/"
+                            className="text-blue-600 hover:underline"
+                        >
+                            Spaghetti with Bean Balls - Vegan Heaven
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            href="https://lovingitvegan.com/baked-vegan-mac-and-cheese/"
+                            className="text-blue-600 hover:underline"
+                        >
+                            Baked Vegan Mac and Cheese - Loving It Vegan
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            href="https://rainbowplantlife.com/vegan-miso-butter-mushroom-risotto/"
+                            className="text-blue-600 hover:underline"
+                        >
+                            Vegan Miso Butter Mushroom Risotto - Rainbow Plant Life
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            href="https://minimalistbaker.com/vegan-jalapeno-poppers/"
+                            className="text-blue-600 hover:underline"
+                        >
+                            Vegan Jalapeño Poppers - Minimalist Baker
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            href="https://www.noracooks.com/vegan-spinach-artichoke-dip/"
+                            className="text-blue-600 hover:underline"
+                        >
+                            Vegan Spinach Artichoke Dip - Nora Cooks
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            href="https://runningonrealfood.com/easy-vegan-bruschetta/"
+                            className="text-blue-600 hover:underline"
+                        >
+                            Easy Vegan Bruschetta - Running on Real Food
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            href="https://frommybowl.com/healthy-vegan-onion-rings/"
+                            className="text-blue-600 hover:underline"
+                        >
+                            Healthy Vegan Onion Rings - From My Bowl
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            href="https://www.allrecipes.com/recipe/25273/spinach-balls/"
+                            className="text-blue-600 hover:underline"
+                        >
+                            Spinach Balls - Allrecipes
+                        </a>
+                    </li>
+                </ul>
+            </section>
+
+            <section>
+                <h2 className="text-2xl font-semibold mb-4">Images</h2>
+                <ul className="list-disc pl-5 space-y-2">
+                    <li>
+                        Andie Mitchell. (n.d.). Caesar Salad. Perfect Caesar Salad with
+                        the Best Caesar Dressing. Retrieved January 19, 2025, from{" "}
+                        <a
+                            href="https://www.andiemitchell.com/perfect-caesar-salad-with-the-best-caesar-dressing/"
+                            className="text-blue-600 hover:underline"
+                        >
+                            https://www.andiemitchell.com/perfect-caesar-salad-with-the-best-caesar-dressing/
+                        </a>
+                    </li>
+                    <li>
+                        A Better Choice. (n.d.). Spinach Bites. Retrieved January 19,
+                        2025, from{" "}
+                        <a
+                            href="https://www.abetterchoice.com.au/recipes/spinach-bites/"
+                            className="text-blue-600 hover:underline"
+                        >
+                            https://www.abetterchoice.com.au/recipes/spinach-bites/
+                        </a>
+                    </li>
+                    <li>
+                        Cherrier, C. (n.d.). Tomato Bruschetta. Easy Tomato Bruschetta.
+                        Eatwell 101. Retrieved January 19, 2025, from{" "}
+                        <a
+                            href="https://www.eatwell101.com/easy-tomato-bruschetta-recipe"
+                            className="text-blue-600 hover:underline"
+                        >
+                            https://www.eatwell101.com/easy-tomato-bruschetta-recipe
+                        </a>
+                    </li>
+                    <li>
+                        Elkus, G. (n.d.). Black Bean Veggie Burger. Easy 5-Ingredient
+                        Black Bean Veggie Burgers. The Kitchn. Retrieved January 19, 2025,
+                        from{" "}
+                        <a
+                            href="https://www.thekitchn.com/black-bean-burger-261539"
+                            className="text-blue-600 hover:underline"
+                        >
+                            https://www.thekitchn.com/black-bean-burger-261539
+                        </a>
+                    </li>
+                    <li>
+                        Hanson, L. (n.d.). Eggplant Parmesan. Fit Foodie. Retrieved
+                        January 19, 2025, from{" "}
+                        <a
+                            href="https://fitfoodiefinds.com/the-best-eggplant-parmesan/"
+                            className="text-blue-600 hover:underline"
+                        >
+                            https://fitfoodiefinds.com/the-best-eggplant-parmesan/
+                        </a>
+                    </li>
+                    <li>
+                        Hylton, J. (n.d.). Vegan Spaghetti and Meatballs. Jessica in the
+                        Kitchen. Retrieved January 19, 2025, from{" "}
+                        <a
+                            href="https://jessicainthekitchen.com/vegan-spaghetti-and-meatballs/"
+                            className="text-blue-600 hover:underline"
+                        >
+                            https://jessicainthekitchen.com/vegan-spaghetti-and-meatballs/
+                        </a>
+                    </li>
+                    <li>
+                        Kitchen, E. T. (n.d.). Vegetarian Lasagna. Roasted Vegetable
+                        Lasagna. Eating Well. Retrieved January 19, 2025, from{" "}
+                        <a
+                            href="https://www.eatingwell.com/recipe/263079/roasted-vegetable-lasagna/"
+                            className="text-blue-600 hover:underline"
+                        >
+                            https://www.eatingwell.com/recipe/263079/roasted-vegetable-lasagna/
+                        </a>
+                    </li>
+                    <li>
+                        McDonald, M. (n.d.). Vegan Baked Mac and Cheese. A Virtual Vegan.
+                        Retrieved January 19, 2025, from{" "}
+                        <a
+                            href="https://avirtualvegan.com/baked-vegan-mac-and-cheese/"
+                            className="text-blue-600 hover:underline"
+                        >
+                            https://avirtualvegan.com/baked-vegan-mac-and-cheese/
+                        </a>
+                    </li>
+                    <li>
+                        Nisha. (n.d.). Mushroom Risotto. The Best Vegan Mushroom Risotto.
+                        Rainbow Plant Life. Retrieved January 19, 2025, from{" "}
+                        <a
+                            href="https://rainbowplantlife.com/vegan-miso-butter-mushroom-risotto/"
+                            className="text-blue-600 hover:underline"
+                        >
+                            https://rainbowplantlife.com/vegan-miso-butter-mushroom-risotto/
+                        </a>
+                    </li>
+                    <li>
+                        Our Balanced Bowl. (n.d.). Cauliflower Steaks with Chimichurri
+                        Sauce. Roasted Cauliflower Steaks with Chimichurri Sauce.
+                        Retrieved January 19, 2025, from{" "}
+                        <a
+                            href="https://ourbalancedbowl.com/roasted-cauliflower-steaks-with-chimichurri-sauce/"
+                            className="text-blue-600 hover:underline"
+                        >
+                            https://ourbalancedbowl.com/roasted-cauliflower-steaks-with-chimichurri-sauce/
+                        </a>
+                    </li>
+                    <li>
+                        Phelps, C. T. (n.d.). Vegetarian Stuffed Peppers. Pickled Plum.
+                        Retrieved January 19, 2025, from{" "}
+                        <a
+                            href="https://pickledplum.com/vegetarian-stuffed-peppers/"
+                            className="text-blue-600 hover:underline"
+                        >
+                            https://pickledplum.com/vegetarian-stuffed-peppers/
+                        </a>
+                    </li>
+                    <li>
+                        Natasha's Kitchen. (n.d.). Crispy Onion Rings with Dipping Sauce.
+                        Retrieved January 19, 2025, from{" "}
+                        <a
+                            href="https://natashaskitchen.com/crisp-onion-rings-recipe/"
+                            className="text-blue-600 hover:underline"
+                        >
+                            https://natashaskitchen.com/crisp-onion-rings-recipe/
+                        </a>
+                    </li>
+                </ul>
+            </section>
+        </div>
+    );
+}
